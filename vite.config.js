a9a1845df44d2094d840995e930f4751c134096c
Arielle Castillo <ariellecharlize26@gmail.com>
2026-09-23 18:35:40 +0800

    Configure Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/MyPortfolio/',
   plugins: [vue(), tailwindcss()],
   server: {
     host: '0.0.0.0',
